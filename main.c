968d070cd482e7f4b49464b3d71183de0d77c425
khalilrached <khalilrached058@gmail.com>
2022-04-09 01:50:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ const char* ssid     = "REPLACE_WITH_YOUR_SSID";
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
 // REPLACE with your Domain name and URL path or IP address with path
-const char* serverName = "http://adresse_ip/post-esp-data.php";
+const char* serverName = "http://ip/post-esp-data.php";
 
 // Keep this API Key value to be compatible with the PHP code provided in the project page. 
 // If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
